52baa6336f2e968c12a47aae21fb38e3df1403f9
sampach95 <60448621+sampach95@users.noreply.github.com>
2021-05-27 14:32:17 -0500

    Add files via upload

---
+++ estaciones.js
@@ -0,0 +1,12 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Estacion 1. La Cima II", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.194849220136206, 19.113062004645929, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Estacion 2. Ciclopista", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.197854970303041, 19.11419752764375, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Estacion 5. Volcan", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.161635118707323, 19.108891704300621, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Estacion 3. Complejo de volcanes Guespala", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.182342133798414, 19.091511224809519, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Estacion 4. Pelado", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -99.216776197007945, 19.15140169975524, 0.0 ] } }
+]
+};
